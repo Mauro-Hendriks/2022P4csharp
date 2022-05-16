8c55aa2ef834cce0845ef260b74c33a76d6488a2
Mauro Hendriks <89855665+Mauro-Hendriks@users.noreply.github.com>
2022-05-16 15:17:53 +0200

    list Opdracht 2 (Zilver)

---
+++ ListOpdracht/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListOpdracht
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<string> birds = new List<string>();
+            birds.Add("uil");
+            birds.Add("kraai");
+            birds.Add("hond");
+            birds.Add("papegaai");
+
+            List<string> birds2 = new List<string>();
+            birds2.Add("meeuw");
+            birds2.Add("duif");
+
+            birds.AddRange(birds2);
+
+            birds.Remove("hond");
+
+            foreach (string bird in birds)
+            {
+                Console.WriteLine(bird);
+            }
+        }
+    }
+}
